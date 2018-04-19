28f59e66825d9631e0bbcf77149e064efac7facf
Blagovest Bonchev <priestbaby312@gmail.com>
2018-04-19 23:40:21 +0300

    Strings and Text Processing

---
+++ Strings and Text Processing/Count Substring Occurrences/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Substring_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+            string searchText = Console.ReadLine().ToLower();
+            int count = 0;
+            int index = text.IndexOf(searchText);
+           while(index != -1)
+            {
+                count++;
+                index = text.IndexOf(searchText, index + 1);
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Strings and Text Processing/Palindromes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split(new char[] { ' ', ',', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var palindromes = new List<string>();
+          
+            for (int i = 0; i < text.Length; i++)
+            {
+              
+               if(IsPalindrome(text[i]))
+                {
+                   if(!palindromes.Contains(text[i]))
+                    {
+                        palindromes.Add(text[i]);
+                    }
+                }
+            }
+            palindromes.Sort();
+            Console.WriteLine(string.Join(", ",palindromes));
+}
+        private static bool IsPalindrome(string word)
+        {
+            int rightIndex = word.Length - 1;
+            int leftIndex = 0;
+
+            while (rightIndex >= leftIndex)
+            {
+                if (word[rightIndex] != word[leftIndex])
+                {
+                    return false;
+                }
+
+                rightIndex--;
+                leftIndex++;
+            }
+
+            return true;
+        }
+    }
+}
